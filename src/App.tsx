9127f8776b86a3f1feecfc823cecffd761096117
Korshun <dmitriy.kor91@gmail.com>
2026-03-04 14:20:15 +0200

    add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import './utils/_fonts.scss';
 import './App.scss';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+// import { ProductCard } from './components/ProductCard';
+// import product from '../public/api/products.json';
+import { HashRouter } from 'react-router-dom';
+import { AppRouter } from './router/AppRouter';
+import { FavoritesProvider } from './context/FavoritesContext';
+import { CartProvider } from './context/CartContext';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <HashRouter>
+    <div className="app">
+      <FavoritesProvider>
+      <CartProvider>
+        <Header />
+        <main className="main-content">
+          <div className="container">
+            <AppRouter />
+          </div>
+        </main>
+        <Footer />
+       </CartProvider>
+      </FavoritesProvider>
     </div>
-  );
+    </HashRouter>
+  )
 };
